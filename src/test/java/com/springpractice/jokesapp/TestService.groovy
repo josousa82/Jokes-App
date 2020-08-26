2d05c7ecb830faa29dc8cf7afed220e0077a877d
sousajo <josesousa@rentalcars.com>
2020-08-26 19:43:53 +0100

    Added remote repository to project

--- src/test/java/com/springpractice/jokesapp/TestService.groovy
+++ src/test/java/com/springpractice/jokesapp/TestService.groovy
@@ -1,4 +1,14 @@
 package com.springpractice.jokesapp
 
-class TestService {
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class TestService extends Specification{
+
+    def "let's try this!"() {
+        expect:
+        Math.max(1, 2) == 3
+    }
+
 }
