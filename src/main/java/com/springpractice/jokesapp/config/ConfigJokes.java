aec3f481ac92abd6d7806afb38a12691503f49e8
sousajo <josesousa@rentalcars.com>
2020-09-03 01:52:33 +0100

    Added xml configuration example to ChuckNorrisJokes. Closes #19

--- src/main/java/com/springpractice/jokesapp/config/ConfigJokes.java
+++ src/main/java/com/springpractice/jokesapp/config/ConfigJokes.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
  * Created by sousaJ on 03/09/2020
  * in package - com.springpractice.jokesapp.config
  **/
-@Configuration
+//@Configuration
 public class ConfigJokes {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
